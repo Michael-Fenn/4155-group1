2d5a8c4ab0f1b2d1b1a012f09feb6636b8bfde29
Michael Fenn <mgfenn157@gmail.com>
2022-11-16 15:12:42 -0500

    attempting building color change

--- docs/index.js
+++ docs/index.js
@@ -1,17 +1,23 @@
 // Initialize and add the map
-function initMap() {
-    // The location of Uluru
-    const uluru = { lat: 35.307240101907844, lng: -80.73516286504413 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 15,
-        center: uluru,
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-        position: uluru,
-        map: map,
-    });
-}
 
-window.initMap = initMap;
+mapboxgl.accessToken = 'pk.eyJ1IjoibWZlbm4yIiwiYSI6ImNsYWp1cGk0aTAzNnUzbnMwZ3o0bm4xNG8ifQ.-FWDnfl7FidedLkI7qIJiA';
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mfenn2/clajw6p60001j14qphmp5iz3n', // style URL
+    zoom: 9 // starting zoom
+});
+
+//placeholder values
+var very_high = 1000;
+var high = 800;
+var medium = 600;
+var low = 400;
+var very_low = 200;
+
+map.setPaintProperty("buildings", "fill-color",
+    ["case",
+        ["==", ["id"], 1060166212],
+        "ff0000",
+        "#00000"]);
+
+
