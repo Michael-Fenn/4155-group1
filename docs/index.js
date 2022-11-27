beaa0473a93644168983dc89d865e52e285f451e
Michael Fenn <mgfenn157@gmail.com>
2022-11-27 16:18:12 -0500

    added popup on click feature

--- docs/index.js
+++ docs/index.js
@@ -21,4 +21,21 @@ map.on('load', function() {
   map.setPaintProperty("woodward", "fill-color", "#FFB52E");
 });
 
+map.on('click', (event) => {
+  const features = map.queryRenderedFeatures(event.point, {
+    layers: ['portal'] // replace with your layer name
+  });
+  if (!features.length) {
+    return;
+  }
+  const feature = features[0];
+
+  const popup = new mapboxgl.Popup({ offset: [0, -15] })
+  .setLngLat(feature.geometry.coordinates)
+  .setHTML(
+    `<h3>${feature.properties.name}</h3><p>${feature.properties.amount}</p>`
+  )
+  .addTo(map);
+});
+
  
